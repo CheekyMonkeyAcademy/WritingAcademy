5462e24bc58c8dd2aeacc460c28392e46b48cdcf
neliza <nicolasaelf64@gmail.com>
2017-10-31 14:50:05 -0600

    rough outline of routes needed

---
+++ routes/genreApiSearchRoute.js
@@ -0,0 +1,3 @@
+//app.get("")
+    //Search a story based off of specific genres tagged to the story
+    //The result will kick back a list of stories
---
+++ routes/storyApiRoutes.js
@@ -0,0 +1,31 @@
+// POST--CREATE
+// GET--READ
+// PUT--UPDATE
+// DELETE--DELETE
+
+//ROUTING PSEUDO-CODE
+
+    //ROOT Page???
+
+    //Create a story
+        //app.post("")
+        //Route to create story
+
+    //Update a story
+        //UPDATE WILL NEED BOTH A GET AND A PUT ROUTE
+       //app.get("")
+        //This will get the specific story with an id passed in its parameters
+
+        //app.put("")
+            //This is handle the specifics of updating the actual story
+
+    //Read
+        //app.get("")
+        //Route to read a specific story id
+            //Includes lines from said story
+    
+    //***** */
+    //DELETE a story?? Maybe??
+    //Possibly hide story or update it to where it is no longer visible???
+    //Global hide variable
+
