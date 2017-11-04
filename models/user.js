2b92190ede8c666d81d3cdc561f18b06c3e0dd61
Robert Rollner <robrollner@gmail.com>
2017-11-03 22:48:15 -0600

    added some basic local logins doesn't error but isn't posting to DB

--- models/user.js
+++ models/user.js
@@ -2,9 +2,49 @@
 
 module.exports = function(sequelize, DataTypes) {
     let User = sequelize.define("User", {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER
+        },
+
+        firstname: {
+            type: DataTypes.STRING,
+            notEmpty: true
+        },
+
+        lastname: {
+            type: DataTypes.STRING,
+            notEmpty: true
+        },
+
+        username: {
+            type: DataTypes.TEXT
+        },
+
+        email: {
+            type: DataTypes.STRING,
+            validate: {
+                isEmail: true
+            }
+        },
+
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        last_login: {
+            type: DataTypes.DATE
+        },
+
+        status: {
+            type: DataTypes.ENUM('active', 'inactive'),
+            defaultValue: 'active'
+        }
         // Giving the User model a name of type STRING
-        name: DataTypes.STRING
-            //may need to expand on the User object and set some variables
+        // name: DataTypes.STRING
+        //may need to expand on the User object and set some variables
 
     });
 
