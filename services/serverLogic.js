5619d655a1395eee1371e0fe5101794f64e3e9a9
Kyle Kowalski <Kyle.Kowalski@gmail.com>
2017-11-07 14:10:18 -0700

    logic is coming together nicely

--- services/serverLogic.js
+++ services/serverLogic.js
@@ -1,8 +1,11 @@
-// goal:  toggle between writing and voting
+// goal:  automagic toggle between writing and voting
+    // should we hide this ENTIRELY from user view?  
 
 // goal:  if voting tied, return tied and do not toggle
 
 // goal:  if not met min voters or min writers, return and do not toggle
+
+// goal:  handle a tie scenario for voting
 
 // stretch goal:  setup a schedule - suggest:  https://www.npmjs.com/package/timers
 var db = require("../models");
@@ -21,25 +24,43 @@ function toggleWritingAndVoting(storyId){
         }
     }).then(function(thisStory){
         console.log('******************* toggleWritingAndVoting Then Clause');
-        // console.log(thisStory.dataValues);
-        if (checkMinRequirementsMet(thisStory.dataValues)){
-            console.log(`We've met the min requiresment to continue`);
+        let writingTrueOrVotingFalse = thisStory.dataValues.toggleWritingTrueOrVotingFalse;
+        console.log(`We're currently at writing true voting false of: ${writingTrueOrVotingFalse}`);
+        if (writingTrueOrVotingFalse){
+            console.log(`We are currently writing, let's see if we are ready to switch to voting.`);
+            if (checkMinWriters(thisStory.dataValues)){
+                console.log(`Switching from writing to voting`);
+                // It's time to switch from writing to voting:
+                thisStory.update({toggleWritingTrueOrVotingFalse: false});
+            }
+            else {
+                console.log(`We're not quite ready to switch from writing to voting`)
+                // TODO kick off some sort of 'add more writers' ?
+            }
+        }
+        else if (!(writingTrueOrVotingFalse)){
+            if (checkMinVotes(thisStory.dataValues)){
+                console.log(`Ready to switch from voting to writing again - let's insure we're not tied`);
+                if (checkVotesToSeeIfNotTied(thisStory.dataValues)){
+                    console.log(`We're not tied - so we're good to go`);
+                    // Mark all lines as voted on
+                    markAllLinesAsVotedOn(thisStory.dataValues);
+                    // It's time to switch from voting to writing
+                    thisStory.update({toggleWritingTrueOrVotingFalse: true});
+                }
+                else {
+                    // TODO kick off our 'we are tied' logic - this needs to be built.  
+                }
+            }
+            else {
+                console.log(`We're not quite ready to switch from voting to writing`);
+                // TODO kick off some sort of 'add more voters' ?
+            }
+        }
+        else {
+            console.log(`ERROR in writingTrueOrVotingFalse - somehow we were neither true nor false`);
         }
     });
-}
-
-function checkMinRequirementsMet(storyObject){
-    let returnValue = false;
-    console.log('################### checkMinRequirementsMet');
-    
-    // first we see if we have the writers
-    if(checkMinWriters(storyObject)){
-        // if we have the writers, check the votes
-        if(checkMinVotes(storyObject)){
-            returnValue = true;
-        }
-    }
-    return returnValue;
 }
 
 function checkMinVotes(storyObject){
@@ -56,6 +77,18 @@ function checkMinVotes(storyObject){
         }
     });
     return returnValue;
+}
+
+function markAllLinesAsVotedOn(storyObject){
+    // Since we've selected a line that is our winner
+    storyObject.Lines.forEach(function(line){
+        console.log(`Checking line ID: ${line.dataValues.id} - currently voted on of: ${line.dataValues.lineVotedOn}`)
+        line.update({lineVotedOn: true});
+    });
+}
+
+function markLineAsWinner(storyObject){
+    return 1;
 }
 
 function checkMinWriters(storyObject){
