6a0bdf520ef72fef49c2c1c106dc4db8cacf2a53
Robert Rollner <robrollner@gmail.com>
2017-11-06 20:11:32 -0700

    added reddit logins

--- routes/logins.js
+++ routes/logins.js
@@ -1,4 +1,5 @@
 var passport = require('passport');
+var crypto = require('crypto');
 
 
 module.exports = function(app) {
@@ -33,6 +34,26 @@ module.exports = function(app) {
             successRedirect: '/',
             failureRedirect: '/login'
         }));
+
+    app.get('/auth/reddit', function(req, res, next) {
+        req.session.state = crypto.randomBytes(32).toString('hex');
+        passport.authenticate('reddit', {
+            state: req.session.state,
+            duration: 'permanent',
+        })(req, res, next);
+    });
+
+    app.get('/auth/reddit/callback', function(req, res, next) {
+        // Check for origin via state token
+        if (req.query.state == req.session.state) {
+            passport.authenticate('reddit', {
+                successRedirect: '/',
+                failureRedirect: '/login'
+            })(req, res, next);
+        } else {
+            next(new Error(403));
+        }
+    });
 
 
 }
