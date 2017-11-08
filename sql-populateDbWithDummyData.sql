e0cacfe50c0660dc81dd72168eaeb2fdc0b46377
Kyle Kowalski <Kyle.Kowalski@gmail.com>
2017-11-08 12:41:59 -0700

    ummm not a hell of a lot of changes needed, to behonest

--- sql-populateDbWithDummyData.sql
+++ sql-populateDbWithDummyData.sql
@@ -1,18 +1,19 @@
-use writing_db;
+-- use writing_db;
+-- use mnvrsaf2rl6441ma;
 
-INSERT INTO `writing_db`.`stories` (`title`, `genre`, `createdAt`, `UpdatedAt`) VALUES ('The Test Story', 'adventure', '2014-01-04', '2015-02-15'); 
-INSERT INTO `writing_db`.`stories` (`title`,  `genre`,`createdAt`, `UpdatedAt`) VALUES ('Wweeeee win', 'adventure','2014-01-04', '2015-02-15');
+INSERT INTO `Stories` (`title`, `genre`, `createdAt`, `UpdatedAt`) VALUES ('The Test Story', 'adventure', '2014-01-04', '2015-02-15'); 
+INSERT INTO `Stories` (`title`,  `genre`,`createdAt`, `UpdatedAt`) VALUES ('Wweeeee win', 'adventure','2014-01-04', '2015-02-15');
 
-select * from stories;
+select * from Stories;
 
-INSERT INTO `writing_db`.`lines` (`lineText`, `lineOrder`, `lineSelected`, `createdAt`, `updatedAt`, `StoryId`, `lineVotedOn`, `lineVoteCount`) VALUES ('Oleg wrote this line, it won', '1', '1', '2017-1-1', '2017-1-1', '1', '1', '0');
-INSERT INTO `writing_db`.`lines` (`lineText`, `lineOrder`, `lineSelected`, `createdAt`, `updatedAt`, `StoryId`, `lineVotedOn`, `lineVoteCount`) VALUES ('Kyle wrote this line, it lost', '1', '0', '2017-1-1', '2017-1-1', '1', '1', '0');
-INSERT INTO `writing_db`.`lines` (`lineText`, `lineOrder`, `lineSelected`, `createdAt`, `updatedAt`, `StoryId`, `lineVotedOn`, `lineVoteCount`) VALUES ('Someone else wrote this, it lost', '1', '0', '2017-1-1', '2017-1-1', '1', '1', '0');
-INSERT INTO `writing_db`.`lines` (`lineText`, `lineOrder`, `lineSelected`, `createdAt`, `updatedAt`, `StoryId`, `lineVotedOn`, `lineVoteCount`) VALUES ('Kyle wrote this line, it won', '2', '1', '2017-1-1', '2017-1-1', '1', '1', '0');
-INSERT INTO `writing_db`.`lines` (`lineText`, `lineOrder`, `lineSelected`, `createdAt`, `updatedAt`, `StoryId`, `lineVotedOn`, `lineVoteCount`) VALUES ('Mr Green used the candlestick?  Wrong.', '2', '0', '2017-1-1', '2017-1-1', '1', '1', '0');
-INSERT INTO `writing_db`.`lines` (`lineText`, `lineOrder`, `lineSelected`, `createdAt`, `updatedAt`, `StoryId`, `lineVotedOn`, `lineVoteCount`) VALUES ('Once there was a boy who... this line lost', '2', '0', '2017-1-1', '2017-1-1', '1', '1', '0');
-INSERT INTO `writing_db`.`lines` (`lineText`, `lineOrder`, `lineSelected`, `createdAt`, `updatedAt`, `StoryId`, `lineVotedOn`, `lineVoteCount`) VALUES ('Kyle: line to vote on', '3', '0', '2017-1-1', '2017-1-1', '1', '0', '0');
-INSERT INTO `writing_db`.`lines` (`lineText`, `lineOrder`, `lineSelected`, `createdAt`, `updatedAt`, `StoryId`, `lineVotedOn`, `lineVoteCount`) VALUES ('Mr Green: line to vote on.', '3', '0', '2017-1-1', '2017-1-1', '1', '0', '0');
-INSERT INTO `writing_db`.`lines` (`lineText`, `lineOrder`, `lineSelected`, `createdAt`, `updatedAt`, `StoryId`, `lineVotedOn`, `lineVoteCount`) VALUES ('Once there was a boy who... line to vote on', '3', '0', '2017-1-1', '2017-1-1', '1', '0', '0');
+INSERT INTO `Lines` (`lineText`, `lineOrder`, `lineSelected`, `createdAt`, `updatedAt`, `StoryId`, `lineVotedOn`, `lineVoteCount`) VALUES ('Oleg wrote this line, it won', '1', '1', '2017-1-1', '2017-1-1', '1', '1', '0');
+INSERT INTO `Lines` (`lineText`, `lineOrder`, `lineSelected`, `createdAt`, `updatedAt`, `StoryId`, `lineVotedOn`, `lineVoteCount`) VALUES ('Kyle wrote this line, it lost', '1', '0', '2017-1-1', '2017-1-1', '1', '1', '0');
+INSERT INTO `Lines` (`lineText`, `lineOrder`, `lineSelected`, `createdAt`, `updatedAt`, `StoryId`, `lineVotedOn`, `lineVoteCount`) VALUES ('Someone else wrote this, it lost', '1', '0', '2017-1-1', '2017-1-1', '1', '1', '0');
+INSERT INTO `Lines` (`lineText`, `lineOrder`, `lineSelected`, `createdAt`, `updatedAt`, `StoryId`, `lineVotedOn`, `lineVoteCount`) VALUES ('Kyle wrote this line, it won', '2', '1', '2017-1-1', '2017-1-1', '1', '1', '0');
+INSERT INTO `Lines` (`lineText`, `lineOrder`, `lineSelected`, `createdAt`, `updatedAt`, `StoryId`, `lineVotedOn`, `lineVoteCount`) VALUES ('Mr Green used the candlestick?  Wrong.', '2', '0', '2017-1-1', '2017-1-1', '1', '1', '0');
+INSERT INTO `Lines` (`lineText`, `lineOrder`, `lineSelected`, `createdAt`, `updatedAt`, `StoryId`, `lineVotedOn`, `lineVoteCount`) VALUES ('Once there was a boy who... this line lost', '2', '0', '2017-1-1', '2017-1-1', '1', '1', '0');
+INSERT INTO `Lines` (`lineText`, `lineOrder`, `lineSelected`, `createdAt`, `updatedAt`, `StoryId`, `lineVotedOn`, `lineVoteCount`) VALUES ('Kyle: line to vote on', '3', '0', '2017-1-1', '2017-1-1', '1', '0', '0');
+INSERT INTO `Lines` (`lineText`, `lineOrder`, `lineSelected`, `createdAt`, `updatedAt`, `StoryId`, `lineVotedOn`, `lineVoteCount`) VALUES ('Mr Green: line to vote on.', '3', '0', '2017-1-1', '2017-1-1', '1', '0', '0');
+INSERT INTO `Lines` (`lineText`, `lineOrder`, `lineSelected`, `createdAt`, `updatedAt`, `StoryId`, `lineVotedOn`, `lineVoteCount`) VALUES ('Once there was a boy who... line to vote on', '3', '0', '2017-1-1', '2017-1-1', '1', '0', '0');
 
-SELECT * FROM writing_db.`lines`;
+SELECT * FROM `Lines`;
