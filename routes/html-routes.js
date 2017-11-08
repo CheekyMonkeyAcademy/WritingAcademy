aaabf9fdb976de680df02ffe5749a7673ad94789
KOM2005 <oleh_kmyta@comcast.net>
2017-11-07 18:11:14 -0700

    removed lines 27-31 html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -24,11 +24,6 @@ module.exports = function(app) {
        res.sendFile(path.join(__dirname, "../public/test.html"));
     })
 
-    //Create new story
-    app.get('/createStory', function (req, res) {
-        res.render("createStory");
-    })    
-
     //This route goes to the update form
     app.get("/updateStoryForm/:id", function(req, res){
         db.Story.findOne({
